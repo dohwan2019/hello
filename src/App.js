827252a6ddb37e73f93a2189aa9a53e15396a19f
dohwan <goodkandy@naver.com>
2019-01-29 16:29:43 +0900

    ffff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-import LectureGoalList from './components/LectureGoalList';
+import Todos from './components/Todos';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <LectureGoalList/>
+       <Todos title={"강의목표"}
+       items={[
+         "React 개발에 필요한 환경을 구축한다.",
+         "새로운 자바스크립트 문법을 익힌다.",
+         "개발 편의를 위한 VSCode IDE를 익힌다.",
+         "기본적인 Git 사용법을 익힌다.",
+         "PR 코드 리뷰를 응용한 개발 프로세스를 익힌다.",
+         "React 로 간단한 노트 앱을 만들어본다."
+         ]}/>
       </div>
     );
   }
